628dfc27f3920ed2a69ab274423469ff0cf1e551
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-01-22 12:30:16 +0600

    shadcn installed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,31 +1,37 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        primary: "#FF9CE7",
-        secondary: "",
-        success: "#00FFAC",
-        info: "#1C57FF",
-        warning: "#FCEA08",
-        danger: "#FF5700",
-      },
-      container: {
-        center: true,
-        padding: "1rem",
-        screens: {
-          default: "1440px",
-        },
-      },
-    },
+  	extend: {
+  		colors: {
+  			background: 'var(--background)',
+  			foreground: 'var(--foreground)',
+  			primary: '#FF9CE7',
+			secondary: '',
+  			success: '#00FFAC',
+  			info: '#1C57FF',
+  			warning: '#FCEA08',
+  			danger: '#FF5700'
+  		},
+  		container: {
+  			center: true,
+  			padding: '1rem',
+  			screens: {
+  				default: '1440px'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
