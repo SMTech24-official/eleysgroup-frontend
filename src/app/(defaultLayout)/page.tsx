231a6e02187e66c7c8b79ae3d78a1972202af4b5
Meraj Faizan <mdmaraj1500@gmail.com>
2025-01-22 12:41:36 +0600

    fixed error

--- src/app/(defaultLayout)/page.tsx
+++ src/app/(defaultLayout)/page.tsx
@@ -2,7 +2,7 @@ import { Banner } from "@/components/home/Banner";
 
 const CommonLayoutHomePage = () => {
   return (
-    <div className="container">
+    <div>
       <Banner />
     </div>
   );
--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -1,5 +1,4 @@
 import { Button } from "@/components/ui/button";
-import moduleName from 'module';
 
 export function Banner() {
   return (
