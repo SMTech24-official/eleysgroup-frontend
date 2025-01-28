88a51cf644fbe9ac624db88422ad7ce9520375b5
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-01-28 19:52:48 +0600

    login completed

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,8 +1,18 @@
-import { ReactNode } from "react";
+"use client";
+import { ReactNode, useEffect } from "react";
 import Sidebar from "@/components/dashboard/Sidebar";
+import { useRouter } from "next/navigation";
 
 const DashboardLayout = ({ children }: { children: ReactNode }) => {
+  const router = useRouter();
 
+  useEffect(() => {
+    // check for accessToken in localStorage . If not found redirect to login page
+    const accessToken = localStorage.getItem("accessToken");
+    if (!accessToken) {
+      router.push("/login");
+    }
+  }, [router]);
 
   return (
     <div className="flex">
--- src/components/dashboard/Sidebar.tsx
+++ src/components/dashboard/Sidebar.tsx
@@ -5,6 +5,7 @@ import { FcMenu } from "react-icons/fc";
 import { RxCross2 } from "react-icons/rx";
 import { usePathname } from "next/navigation";
 import { MdOutlineDashboard } from "react-icons/md";
+import { Button } from "../ui/button";
 
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -93,6 +94,22 @@ const Sidebar = () => {
               >
                 All Slots
               </Link>
+            </li>
+            {/* lgoout button  */}
+            <li className="flex gap-4 justify-start items-center">
+              <MdOutlineDashboard size={24} />
+              <Button
+                onClick={() => {
+                  localStorage.removeItem("accessToken");
+                  window.location.href = "/login";
+                }}
+                variant={"destructive"}
+                className={`flex items-center  space-x-2 p-2 rounded hover:bg-gray-700 hover:text-white hover:border-none w-full font-semibold 
+                  ${pathname === "/dashboard/logout" ? "bg-white text-primary" : "border border-white text-white"}
+                `}
+              >
+                Logout
+              </Button>
             </li>
           </ul>
         </nav>
