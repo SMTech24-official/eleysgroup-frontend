b82555b35204d16bcf470ad2160db0d5ca2216fd
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-02-02 15:42:57 +0600

    refactor tailwind configuration for improved readability and organization

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,59 +1,59 @@
 import type { Config } from "tailwindcss";
 import tailwindcssAnimate from "tailwindcss-animate";
 export default {
-    darkMode: ["class"],
-    content: [
+  darkMode: ["class"],
+  content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-  	extend: {
-  		colors: {
-  			background: 'var(--background)',
-  			foreground: 'var(--foreground)',
-  			primary: '#FF9CE7',
-  			secondary: '',
-  			success: '#00FFAC',
-  			info: '#1C57FF',
-  			warning: '#FCEA08',
-  			danger: '#FF5700'
-  		},
-  		container: {
-  			center: true,
-  			padding: '1rem',
-  			screens: {
-  				default: '1200px'
-  			}
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		keyframes: {
-  			'accordion-down': {
-  				from: {
-  					height: '0'
-  				},
-  				to: {
-  					height: 'var(--radix-accordion-content-height)'
-  				}
-  			},
-  			'accordion-up': {
-  				from: {
-  					height: 'var(--radix-accordion-content-height)'
-  				},
-  				to: {
-  					height: '0'
-  				}
-  			}
-  		},
-  		animation: {
-  			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out'
-  		}
-  	}
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        primary: "#FF9CE7",
+        secondary: "",
+        success: "#00FFAC",
+        info: "#1C57FF",
+        warning: "#FCEA08",
+        danger: "#FF5700",
+      },
+      container: {
+        center: true,
+        padding: "1rem",
+        screens: {
+          default: "1200px",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
+        },
+        "accordion-up": {
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
   },
   plugins: [tailwindcssAnimate],
 } satisfies Config;
